c9a1ad3bc7bafb58d2d8c0947c72c0c8f7569535
habc706 <71514169+habc706@users.noreply.github.com>
2021-11-14 23:47:34 +0800

    新增分数表，分数接口

--- api/api/settings.py
+++ api/api/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',  #该语句会检测post请求的安全性
+    'django.middleware.csrf.CsrfViewMiddleware',  #该语句会检测post请求的安全性
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- api/database/models.py
+++ api/database/models.py
@@ -7,22 +7,17 @@ class wuser_info(models.Model):
     name = models.CharField(max_length=6,null=True)
     role = models.CharField(null=False, default='0',max_length=1)  # 如果是学生就1老师是2游客是0
 
-class stu(models.Model):
-    stu_id = models.CharField(primary_key=True,max_length=30)
-
-    wsex=models.CharField(null=True,max_length=1)
-
-
-class tea(models.Model):
-    tea_id=models.CharField(max_length=1,primary_key=True)
-
-    wsex=models.CharField(null=True,max_length=1)
-
 
 class course(models.Model):
     course_id = models.CharField(primary_key=True,max_length=30)
     cou_info = models.CharField(max_length=300)
     cou_type = models.CharField(max_length=1)
+
+class course_score(models.Model):
+    cou_id = models.CharField(primary_key=True,max_length=30)  # 课程id
+    stud_id = models.CharField(max_length=30)  # 学生id
+    number = models.IntegerField()  # 第几次的
+    real_score = models.IntegerField()  # 实际分数
 
 
 class courese_selected(models.Model):
