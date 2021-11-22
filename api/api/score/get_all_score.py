4e0f0a87f6a3dfce51253d02200041eed35155fd
habc706 <71514169+habc706@users.noreply.github.com>
2021-11-22 19:15:15 +0800

    修改刷新接口

--- api/api/score/get_all_score.py
+++ api/api/score/get_all_score.py
@@ -5,7 +5,6 @@ from database.models import course_score
 
 '''
 此接口接受post请求，请求参数位openid，course_id,请求的对象是学生,返回的是，按照次序排列号的分数数组
-感觉那个页面的下拉刷新可以采取一样的接口,
 '''
 
 class get_all_score(APIView):
@@ -13,11 +12,13 @@ class get_all_score(APIView):
         openid = request.data.get('openid')
         course_id = request.data.get('course_id')
         score_list = []
+        evaluate_list = []
         my_this_score = course_score.objects.filter(stud_id=openid,cou_id = course_id)
         my_order_score = my_this_score.order_by('number')  # 按照number拍好次序
         for var in my_order_score:
             score_list.append(var.real_score)
-
+            evaluate_list.append(var.evaluate)
         return Response({
-            'score_list':score_list
+            'score_list':score_list,
+            'evaluate_list':evaluate_list
         })
